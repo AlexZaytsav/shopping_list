f18841b020198931898ce1df37a674b1982c11ba
AlexZaytsev <zayt.alex@gmail.com>
2013-11-19 15:15:33 +0300

    backbone - adding model

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 (function ($){
 
-//    var Item = Backbone.Model.extend({
-//
-//        defaults: function(){
-//            return {
-//                name: 'empty name',
-//                price: 'empty price',
-//                purchase_date: 'empty date',
-//                expiration_date: 'empty date'
-//            }
-//        },
-//
-//        toggle: function() {
-//            this.save();
-//        }
-//    });
+    var Item = Backbone.Model.extend({
 
-    var ItemView = Backbone.View.extend({
+        defaults: function(){
+            return {
+                name: 'empty name',
+                price: 'empty price',
+                purchase_date: 'empty date',
+                expiration_date: 'empty date'
+            }
+        }
+    });
+
+    var List = Backbone.Collection.extend({
+        model: Item
+    });
+
+    var ListView = Backbone.View.extend({
         el: 'body', // attaches `this.el` to an existing element.
 
         events: {
@@ -24,20 +24,44 @@
         },
 
         initialize: function(){
-            _.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+            _.bindAll(this, 'render', 'addItem', 'appendItem'); // fixes loss of context for 'this' within methods
+
+            this.collection = new List();
+            this.collection.bind('add', this.appendItem); // collection event binder
 
             this.render(); // not all views are self-rendering. This one is.
         },
 
         render: function(){
-            $(this.el).find('#item_list').append("<ul> <li>hello world</li> </ul>");
+            var self = this;
+
+            _(this.collection.models).each(function(item){ // in case collection is not empty
+                self.appendItem(item);
+            }, this);
         },
 
         addItem: function(){
-            $(this.el).find('#item_list').append("<ul> <li>hello world</li></ul>");
+            var item = new Item();
+
+            item.set({
+                name: item.get('name'), // modify item defaults
+                price: item.get('price'),
+                purchase_date: item.get('purchase_date'),
+                expiration_date: item.get('expiration_date')
+            });
+
+            this.collection.add(item); // add item to collection; view is updated via event 'add'
+        },
+
+        appendItem: function(item){
+            $(this.el).find('#item_list').append("<tr><td>" + item.get('name') + "</td>" +
+                                                     "<td>" + item.get('price') + "</td>" +
+                                                     "<td>" + item.get('purchase_date') + "</td>" +
+                                                     "<td>" + item.get('expiration_date') + "</td></tr>");
         }
+
     });
 
-    var itemView = new ItemView();
+    var listView = new ListView();
 
 })(jQuery);
