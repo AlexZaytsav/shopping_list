e529fd474ddf3f8e4479e564e5be1f2b67f51602
AlexZaytsev <zayt.alex@gmail.com>
2013-11-19 15:49:17 +0300

    backbone - adding item view

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-(function ($){
+(function ($) {
 
     var Item = Backbone.Model.extend({
 
-        defaults: function(){
+        defaults: function () {
             return {
                 name: 'empty name',
                 price: 'empty price',
@@ -16,6 +16,22 @@
         model: Item
     });
 
+    var ItemView = Backbone.View.extend({
+
+        el: 'body', // name of (orphan) root tag in this.el
+
+        initialize: function () {
+            _.bindAll(this, 'render'); // every function that uses 'this' as the current object should be in here
+        },
+        render: function () {
+            $(this.el).find('#item_list').append("<tr><td>" + this.model.get('name') + "</td>" +
+                "<td>" + this.model.get('price') + "</td>" +
+                "<td>" + this.model.get('purchase_date') + "</td>" +
+                "<td>" + this.model.get('expiration_date') + "</td></tr>");
+            return this; // for chainable calls, like .render().el
+        }
+    });
+
     var ListView = Backbone.View.extend({
         el: 'body', // attaches `this.el` to an existing element.
 
@@ -23,7 +39,7 @@
             'click a#add_item': 'addItem'
         },
 
-        initialize: function(){
+        initialize: function () {
             _.bindAll(this, 'render', 'addItem', 'appendItem'); // fixes loss of context for 'this' within methods
 
             this.collection = new List();
@@ -32,15 +48,15 @@
             this.render(); // not all views are self-rendering. This one is.
         },
 
-        render: function(){
+        render: function () {
             var self = this;
 
-            _(this.collection.models).each(function(item){ // in case collection is not empty
+            _(this.collection.models).each(function (item) { // in case collection is not empty
                 self.appendItem(item);
             }, this);
         },
 
-        addItem: function(){
+        addItem: function () {
             var item = new Item();
 
             item.set({
@@ -53,12 +69,14 @@
             this.collection.add(item); // add item to collection; view is updated via event 'add'
         },
 
-        appendItem: function(item){
-            $(this.el).find('#item_list').append("<tr><td>" + item.get('name') + "</td>" +
-                                                     "<td>" + item.get('price') + "</td>" +
-                                                     "<td>" + item.get('purchase_date') + "</td>" +
-                                                     "<td>" + item.get('expiration_date') + "</td></tr>");
+        appendItem: function (item) {
+            var itemView = new ItemView({
+                model: item
+            });
+
+            $(this.el).append(itemView.render().el)
         }
+
 
     });
 
